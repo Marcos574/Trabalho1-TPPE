a5bc94ced9bf98f44814737057ef5390dbcd4290
Marcos Deus <87666623+Marcos574@users.noreply.github.com>
2024-06-30 20:33:49 -0300

    Refatorando testes de endereco
    Refatorando os testes de endereco utilizando as estratégias de parâmetrização com triangulação

--- src/tests/EnderecoTest.java
+++ src/tests/EnderecoTest.java
@@ -1,31 +1,47 @@
 package tests;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
 
 import main.Endereco;
 
 import static org.junit.Assert.*;
 
+import java.util.Arrays;
+import java.util.Collection;
+
+@RunWith(Parameterized.class)
 public class EnderecoTest {
+
+    private String estado;
+    private boolean capital;
+    private boolean expectedCapital;
+
+    public EnderecoTest(String estado, boolean capital, boolean expectedCapital) {
+        this.estado = estado;
+        this.capital = capital;
+        this.expectedCapital = expectedCapital;
+    }
+
+    @Parameters
+    public static Collection<Object[]> data() {
+        return Arrays.asList(new Object[][]{
+            {"SP", true, true},
+            {"RJ", false, false},
+            {"DF", true, true},
+            {"MG", false, false},
+            {"BA", true, true},
+            {"RS", false, false}
+        });
+    }
 
     @Test
     public void testEnderecoCreation() {
-        Endereco endereco = new Endereco("SP", true);
+        Endereco endereco = new Endereco(estado, capital);
         assertNotNull(endereco);
-    }
-
-    @Test
-    public void testGetEstado() {
-        Endereco endereco = new Endereco("RJ", false);
-        assertEquals("RJ", endereco.getEstado());
-    }
-
-    @Test
-    public void testIsCapital() {
-        Endereco endereco1 = new Endereco("DF", true);
-        assertTrue(endereco1.isCapital());
-
-        Endereco endereco2 = new Endereco("MG", false);
-        assertFalse(endereco2.isCapital());
+        assertEquals(estado, endereco.getEstado());
+        assertEquals(expectedCapital, endereco.isCapital());
     }
 }
