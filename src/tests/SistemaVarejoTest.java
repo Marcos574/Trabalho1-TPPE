676b5ee1dd18a52c5f52adb78c867e896ca59ee3
Marcos Deus <87666623+Marcos574@users.noreply.github.com>
2024-06-29 21:16:48 -0300

    TDD - Passo 1
    Criando o teste para o cadastro de cliente, o teste deve falhar

---
+++ src/tests/SistemaVarejoTest.java
@@ -0,0 +1,22 @@
+package tests;
+
+import main.Cliente;
+import main.Endereco;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class SistemaVarejoTest {
+
+    @Test
+    public void deveCadastrarCliente() {
+        SistemaVarejo sistema = new SistemaVarejo();
+        Endereco endereco = new Endereco("SP", true);
+        Cliente cliente = new Cliente("João", "padrão", endereco);
+        
+        sistema.cadastrarCliente(cliente);
+        
+        assertEquals(1, sistema.getClientes().size());
+        assertEquals(cliente, sistema.getClientes().get(0));
+    }
+}
+
