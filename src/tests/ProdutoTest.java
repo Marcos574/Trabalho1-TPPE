ffa5207a17a4a999fcd86d0fd15ef41f24efacee
Marcos Deus <87666623+Marcos574@users.noreply.github.com>
2024-06-30 20:37:27 -0300

    Refatorando os testes de produto
    Refatorando os testes da classe produto utilizando parâmetrização e triangulação

--- src/tests/ProdutoTest.java
+++ src/tests/ProdutoTest.java
@@ -1,18 +1,45 @@
 package tests;
 
-import static org.junit.Assert.assertEquals;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+
 import main.Produto;
 
+import static org.junit.Assert.*;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+@RunWith(Parameterized.class)
 public class ProdutoTest {
+
+    private String codigo;
+    private String descricao;
+    private double valorVenda;
+    private String unidade;
+
+    public ProdutoTest(String codigo, String descricao, double valorVenda, String unidade) {
+        this.codigo = codigo;
+        this.descricao = descricao;
+        this.valorVenda = valorVenda;
+        this.unidade = unidade;
+    }
+
+    @Parameters
+    public static Collection<Object[]> data() {
+        return Arrays.asList(new Object[][]{
+            {"12345", "Produto Teste 1", 10.0, "unidade"},
+            {"67890", "Produto Teste 2", 20.0, "pacote"},
+            {"11121", "Produto Teste 3", 30.0, "caixa"},
+            {"31415", "Produto Teste 4", 40.0, "litro"},
+            {"16171", "Produto Teste 5", 50.0, "quilo"}
+        });
+    }
 
     @Test
     public void testProdutoConstructor() {
-        String codigo = "12345";
-        String descricao = "Produto Teste";
-        double valorVenda = 10.0;
-        String unidade = "unidade";
-
         Produto produto = new Produto(codigo, descricao, valorVenda, unidade);
 
         assertEquals(codigo, produto.getCodigo());
@@ -23,29 +50,25 @@ public class ProdutoTest {
 
     @Test
     public void testGetCodigo() {
-        String codigo = "12345";
-        Produto produto = new Produto(codigo, "Produto Teste", 10.0, "unidade");
+        Produto produto = new Produto(codigo, descricao, valorVenda, unidade);
         assertEquals(codigo, produto.getCodigo());
     }
 
     @Test
     public void testGetDescricao() {
-        String descricao = "Produto Teste";
-        Produto produto = new Produto("12345", descricao, 10.0, "unidade");
+        Produto produto = new Produto(codigo, descricao, valorVenda, unidade);
         assertEquals(descricao, produto.getDescricao());
     }
 
     @Test
     public void testGetValorVenda() {
-        double valorVenda = 10.0;
-        Produto produto = new Produto("12345", "Produto Teste", valorVenda, "unidade");
+        Produto produto = new Produto(codigo, descricao, valorVenda, unidade);
         assertEquals(valorVenda, produto.getValorVenda(), 0.001);
     }
 
     @Test
     public void testGetUnidade() {
-        String unidade = "unidade";
-        Produto produto = new Produto("12345", "Produto Teste", 10.0, unidade);
+        Produto produto = new Produto(codigo, descricao, valorVenda, unidade);
         assertEquals(unidade, produto.getUnidade());
     }
 }
